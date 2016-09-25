424257d45318c62a61c2a0917dbd313260599d89
Meisterklasse <651893012@qq.com>
2016-09-25 22:25:11 +0800

    Add files via upload

---
+++ 第三次作业L1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+""" 
+for a in range(100):
+    import os
+    i = os.system('cls')    
+    print a*" ","#    #         #####                 ###                    "
+    print a*" "," #  #            #   #              #    #   #              "
+    print a*" ","   #   # #       #      ###  ###    #    #      ###   ##    "  
+    print a*" ","   #   # #     # #   #  # #  # #    #    #   #  # #  #  #   "
+    print a*" ","   #   ###     ###   #  #### # #     ######  #  ####  ##    "
+    import time
+    time.sleep(0.01)
