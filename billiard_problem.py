9179e1d43aecb592f9341dd938df6400fc5a5476
Meisterklasse <651893012@qq.com>
2016-11-21 10:59:10 +0800

    Add files via upload

--- billiard_problem.py
+++ billiard_problem.py
@@ -8,7 +8,7 @@ Created on Sun Nov 20 23:50:23 2016
 import pylab as pl
 import math
 class billiard_problem:
-    def __init__(self,x0=0,y0=-10.0,vx0=1.5,vy0=1.2,dt=0.1):
+    def __init__(self,x0=0,y0=-10.0,vx0=1.5,vy0=1.2,dt=0.01):
         self.x=[x0]
         self.y=[y0]
         self.vx=vx0
@@ -23,7 +23,7 @@ class billiard_problem:
         for i in range(0,360):
             self.xx.append(5*math.cos(1*i))
             self.yy.append(5*math.sin(1*i))
-        for i in range(0,10000):
+        for i in range(0,50000):
             
             
             if self.x[-1]>10 or self.x[-1]<-10:
@@ -32,8 +32,10 @@ class billiard_problem:
                 self.vy=-self.vy
             #正方形外框
             if self.x[-1]**2+self.y[-1]**2<=25:
-                self.vx=(2*self.x[-1]*self.y[-1]*self.vy+(self.x[-1]**2+self.y[-1]**2)*self.vx)/(self.y[-1]**2-self.x[-1]**2)
-                self.vy=(2*self.x[-1]*self.y[-1]*self.vx+(self.x[-1]**2+self.y[-1]**2)*self.vy)/(-self.y[-1]**2+self.x[-1]**2)
+                tempvx=(2*self.x[-1]*self.y[-1]*self.vy+(self.x[-1]**2+self.y[-1]**2)*self.vx)/(self.y[-1]**2-self.x[-1]**2)
+                tempvy=(2*self.x[-1]*self.y[-1]*self.vx+(self.x[-1]**2+self.y[-1]**2)*self.vy)/(-self.y[-1]**2+self.x[-1]**2)
+                self.vx=tempvx
+                self.vy=tempvy
             print self.x[-1]
             print self.y[-1]
             print self.vx
@@ -52,7 +54,7 @@ class billiard_problem:
                 
             self.t.append(self.dt)
     def show_results(self):
-        pl.plot(self.x,self.y,".")
+        pl.plot(self.x,self.y)
         pl.plot(self.xx,self.yy,".")
         pl.xlim(-10,10)
         pl.ylim(-10,10)
